1f3c5c63a80cca760e9aeb125cceaa438876a5ec
m2leal <mr.leal@gmail.com>
2024-06-02 15:56:34 -0300

    Adiciona perfis de Desenvolvimento e Produção

--- src/main/java/com/algaworks/algafood/infrastructure/service/query/VendaQueryServiceImplDev.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/query/VendaQueryServiceImplDev.java
@@ -8,14 +8,16 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
 
+@Profile("dev")
 @Repository
-public class VendaQueryServiceImpl implements VendaQueryService {
+public class VendaQueryServiceImplDev implements VendaQueryService {
 	
 	@PersistenceContext
 	private EntityManager manager;
@@ -58,49 +60,5 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 		
 		return vendasDiarias;
 	}
-	
-//	Versão pra banco MySQL
-//	@Override
-//	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
-//		var builder = manager.getCriteriaBuilder();
-//		var query = builder.createQuery(VendaDiaria.class);
-//		var root = query.from(Pedido.class);
-//		var predicates = new ArrayList<Predicate>();
-//		
-//		var functionConvertTzDataCriacao = builder.function(
-//				"convert_tz", Date.class, root.get("dataCriacao"),
-//				builder.literal("+00:00"), builder.literal(timeOffset));
-//		
-//		var functionDateDataCriacao = builder.function(
-//				"date", Date.class, functionConvertTzDataCriacao);
-//		
-//		var selection = builder.construct(VendaDiaria.class,
-//				functionDateDataCriacao,
-//				builder.count(root.get("id")),
-//				builder.sum(root.get("valorTotal")));
-//		
-//		if (filtro.getRestauranteId() != null) {
-//			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
-//		}
-//		
-//		if (filtro.getDataCriacaoInicio() != null) {
-//			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), 
-//					filtro.getDataCriacaoInicio()));
-//		}
-//		
-//		if (filtro.getDataCriacaoFim() != null) {
-//			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), 
-//					filtro.getDataCriacaoFim()));
-//		}
-//		
-//		predicates.add(root.get("status").in(
-//				StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
-//		
-//		query.select(selection);
-//		query.where(predicates.toArray(new Predicate[0]));
-//		query.groupBy(functionDateDataCriacao);
-//		
-//		return manager.createQuery(query).getResultList();
-//	}
 	
 }
